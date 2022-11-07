31c57a831d5729a0c47b6102b14750d3e2968bef
Iyán Sanjurjo <71704577+iyansl@users.noreply.github.com>
2022-11-07 19:58:39 +0100

    added new changes

--- P3/CalculadoraBasica.js
+++ P3/CalculadoraBasica.js
@@ -3,13 +3,14 @@ class CalculadoraBasica {
 		this.pantalla = "";
 		this.memoria = Number(0);
 	}
+	
 	mrc() {
-		this.pantalla += this.memoria;
+		this.pantalla += Number(this.memoria);
 		document.querySelector("input[type=text]").value = this.pantalla;
 	}
 	mMenos() {
 		try {
-			this.memoria = this.memoria - eval(Number(this.pantalla));
+			this.memoria = this.memoria - eval(this.pantalla);
 		} catch (err) {
 			document.querySelector("input[type=text]").value = "ERROR = " + err;
 			this.pantalla = "";
@@ -17,7 +18,7 @@ class CalculadoraBasica {
 	}
 	mMas() {
 		try {
-			this.memoria = this.memoria + eval(Number(this.pantalla));
+			this.memoria = this.memoria + Number(eval(this.pantalla));
 		} catch (err) {
 			document.querySelector("input[type=text]").value = "ERROR = " + err;
 			this.pantalla = "";
@@ -47,9 +48,18 @@ class CalculadoraBasica {
 		this.pantalla += "/";
 		document.querySelector("input[type=text]").value = this.pantalla;
 	}
+	raiz() {
+		try {
+			this.pantalla = Number(Math.sqrt(Number(eval(this.pantalla))));
+			document.querySelector("input[type=text]").value = this.pantalla;
+		} catch (err) {
+			document.querySelector("input[type=text]").value = "ERROR = " + err;
+			this.pantalla = "";
+		}
+	}
 	igual() {
 		try {
-			this.pantalla = eval(this.pantalla);
+			this.pantalla = Number(eval(this.pantalla));
 			document.querySelector("input[type=text]").value = this.pantalla;
 		} catch (err) {
 			document.querySelector("input[type=text]").value = "ERROR = " + err;
@@ -60,5 +70,49 @@ class CalculadoraBasica {
 		this.pantalla = "";
 		document.querySelector("input[type=text]").value = this.pantalla;
 	}
+	masmenos() {
+		try {
+			this.pantalla = Number(eval(this.pantalla))*-1;
+			document.querySelector("input[type=text]").value = this.pantalla;
+		} catch (err) {
+			document.querySelector("input[type=text]").value = "ERROR = " + err;
+			this.pantalla = "";
+		}
+	}
+	modulo() {
+		this.pantalla += "%";
+		document.querySelector("input[type=text]").value = this.pantalla;
+	}
+	
 }
+
 var calculadoraBasica = new CalculadoraBasica();
+
+document.addEventListener('keydown', (event) => {
+	const keyName = event.key;
+	switch (keyName){
+		case('0'): calculadoraBasica.digitos('0');break;
+		case('1'): calculadoraBasica.digitos('1');break;
+		case('2'): calculadoraBasica.digitos('2');break;
+		case('3'): calculadoraBasica.digitos('3');break;
+		case('4'): calculadoraBasica.digitos('4');break;
+		case('5'): calculadoraBasica.digitos('5');break;
+		case('6'): calculadoraBasica.digitos('6');break;
+		case('7'): calculadoraBasica.digitos('7');break;
+		case('8'): calculadoraBasica.digitos('8');break;
+		case('9'): calculadoraBasica.digitos('9');break;
+		case('+'): calculadoraBasica.suma();break;
+		case('-'): calculadoraBasica.resta();break;
+		case('p'): calculadoraBasica.multiplicacion();break;
+		case('d'): calculadoraBasica.division();break;
+		case('m'): calculadoraBasica.mrc();break;
+		case('n'): calculadoraBasica.mMenos();break;
+		case('o'): calculadoraBasica.mMas();break;
+		case('d'): calculadoraBasica.modulo();break;
+		case('s'): calculadoraBasica.masmenos();break;
+		case('r'): calculadoraBasica.raiz();break;
+		case('.'): calculadoraBasica.punto();break;
+		case('c'): calculadoraBasica.borrar();break;
+		case('i'): calculadoraBasica.igual();break;
+	}
+  });
--- P3/CalculadoraCientifica.js
+++ P3/CalculadoraCientifica.js
@@ -47,6 +47,15 @@ class CalculadoraBasica {
 		this.pantalla += "/";
 		document.querySelector("input[type=text]").value = this.pantalla;
 	}
+	raiz() {
+		try {
+			this.pantalla = Number(Math.sqrt(eval(this.pantalla)));
+			document.querySelector("input[type=text]").value = this.pantalla;
+		} catch (err) {
+			document.querySelector("input[type=text]").value = "ERROR = " + err;
+			this.pantalla = "";
+		}
+	}
 	igual() {
 		try {
 			this.pantalla = eval(this.pantalla);
@@ -58,6 +67,19 @@ class CalculadoraBasica {
 	}
 	borrar() {
 		this.pantalla = "";
+		document.querySelector("input[type=text]").value = this.pantalla;
+	}
+	masmenos() {
+		try {
+			this.pantalla = Number(eval(this.pantalla))*-1;
+			document.querySelector("input[type=text]").value = this.pantalla;
+		} catch (err) {
+			document.querySelector("input[type=text]").value = "ERROR = " + err;
+			this.pantalla = "";
+		}
+	}
+	modulo() {
+		this.pantalla += "%";
 		document.querySelector("input[type=text]").value = this.pantalla;
 	}
 }
@@ -78,15 +100,6 @@ class CalculadoraCientifica extends CalculadoraBasica {
 	secante() {
 		try {
 			this.pantalla = Number(1/Math.cos(eval(this.pantalla)));
-			document.querySelector("input[type=text]").value = this.pantalla;
-		} catch (err) {
-			document.querySelector("input[type=text]").value = "ERROR = " + err;
-			this.pantalla = "";
-		}
-	}
-	cotangente() {
-		try {
-			this.pantalla = Number(1/Math.tan(eval(this.pantalla)));
 			document.querySelector("input[type=text]").value = this.pantalla;
 		} catch (err) {
 			document.querySelector("input[type=text]").value = "ERROR = " + err;
@@ -133,15 +146,6 @@ class CalculadoraCientifica extends CalculadoraBasica {
 			this.pantalla = "";
 		}
 	}
-	raiz() {
-		try {
-			this.pantalla = Number(Math.sqrt(eval(this.pantalla)));
-			document.querySelector("input[type=text]").value = this.pantalla;
-		} catch (err) {
-			document.querySelector("input[type=text]").value = "ERROR = " + err;
-			this.pantalla = "";
-		}
-	}
 	potenciaDiez() {
 		try {
 			this.pantalla = Number(Math.pow(10,eval(this.pantalla)));
@@ -169,10 +173,6 @@ class CalculadoraCientifica extends CalculadoraBasica {
 			this.pantalla = "";
 		}
 	}
-	modulo() {
-		this.pantalla += "%";
-		document.querySelector("input[type=text]").value = this.pantalla;
-	}
 	borrarDigito() {
 		this.pantalla = this.pantalla.substring(0, this.pantalla.length-1);
 		document.querySelector("input[type=text]").value = this.pantalla;
@@ -197,15 +197,6 @@ class CalculadoraCientifica extends CalculadoraBasica {
 		}
 		return Number(total);
 	}
-	absoluto() {
-		try {
-			this.pantalla = Number(Math.abs(eval(this.pantalla)));
-			document.querySelector("input[type=text]").value = this.pantalla;
-		} catch (err) {
-			document.querySelector("input[type=text]").value = "ERROR = " + err;
-			this.pantalla = "";
-		}
-	}
 	parentesisIzquierdo() {
 		this.pantalla += "(";
 		document.querySelector("input[type=text]").value = this.pantalla;
@@ -218,3 +209,47 @@ class CalculadoraCientifica extends CalculadoraBasica {
 
 
 var calculadoraCientifica = new CalculadoraCientifica();
+
+document.addEventListener('keydown', (event) => {
+	const keyName = event.key;
+	switch (keyName){
+		case('0'): calculadoraCientifica.digitos('0');break;
+		case('1'): calculadoraCientifica.digitos('1');break;
+		case('2'): calculadoraCientifica.digitos('2');break;
+		case('3'): calculadoraCientifica.digitos('3');break;
+		case('4'): calculadoraCientifica.digitos('4');break;
+		case('5'): calculadoraCientifica.digitos('5');break;
+		case('6'): calculadoraCientifica.digitos('6');break;
+		case('7'): calculadoraCientifica.digitos('7');break;
+		case('8'): calculadoraCientifica.digitos('8');break;
+		case('9'): calculadoraCientifica.digitos('9');break;
+		case('+'): calculadoraCientifica.suma();break;
+		case('-'): calculadoraCientifica.resta();break;
+		case('p'): calculadoraCientifica.multiplicacion();break;
+		case('d'): calculadoraCientifica.division();break;
+		case('m'): calculadoraCientifica.mrc();break;
+		case('n'): calculadoraCientifica.mMenos();break;
+		case('o'): calculadoraCientifica.mMas();break;
+		case('l'): calculadoraCientifica.modulo();break;
+		case('s'): calculadoraCientifica.masmenos();break;
+		case('r'): calculadoraCientifica.raiz();break;
+		case('.'): calculadoraCientifica.punto();break;
+		case('c'): calculadoraCientifica.borrar();break;
+		case('i'): calculadoraCientifica.igual();break;
+		case('u'): calculadoraCientifica.cuadrado();break;
+		case('t'): calculadoraCientifica.potencia();break;
+		case('e'): calculadoraCientifica.seno();break;
+		case('h'): calculadoraCientifica.coseno();break;
+		case('k'): calculadoraCientifica.secante();break;
+		case('j'): calculadoraCientifica.cosecante();break;
+		case('a'): calculadoraCientifica.tangente();break;
+		case('z'): calculadoraCientifica.potenciaDiez();break;
+		case('g'): calculadoraCientifica.logaritmo();break;
+		case('x'): calculadoraCientifica.exponencial();break;
+		case('b'): calculadoraCientifica.borrarDigito();break;
+		case('q'): calculadoraCientifica.pi();break;
+		case('f'): calculadoraCientifica.factorial();break;
+		case('º'): calculadoraCientifica.parentesisIzquierdo();break;
+		case('¡'): calculadoraCientifica.parentesisDerecho();break;
+	}
+  });
